31dcb72f090a7d55e47cf3ddd13401507072862e
Andrea Formica <andrea.formica@studenti.unimi.it>
2016-05-13 12:26:24 +0200

    Creato server

---
+++ src/main/java/it/unimi/di/sweng/lab08/model/Job.java
@@ -0,0 +1,15 @@
+package it.unimi.di.sweng.lab08.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Job {
+	INSTANCE;
+
+	private final static Map<String, String[]> JOB = new HashMap<String,String[]>();
+	
+	public void loadJobs(Map<String, String[]> jobs) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/main/java/it/unimi/di/sweng/lab08/server/JobsApplication.java
@@ -0,0 +1,7 @@
+package it.unimi.di.sweng.lab08.server;
+
+import org.restlet.Restlet;
+
+public class JobsApplication extends Restlet {
+
+}
