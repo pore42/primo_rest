0f4c7088d5474024e6cd8d885ed4f84841a77b85
Riccardo Crippa <therickys93@gmail.com>
2016-05-15 16:27:57 +0200

    sistemato bug che faceva crashare il server

--- src/main/java/it/unimi/di/sweng/lab08/server/GetJobResource.java
+++ src/main/java/it/unimi/di/sweng/lab08/server/GetJobResource.java
@@ -1,6 +1,9 @@
 package it.unimi.di.sweng.lab08.server;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
+
+import org.restlet.data.Status;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
@@ -11,8 +14,13 @@ public class GetJobResource extends ServerResource {
 	@Get("json")
 	public Map<String, String> getJobInfo() {
 		final String job = getAttribute("name");
+		try {
+			return Job.INSTANCE.getJobInfo(job);
+		} catch (NoSuchElementException e ){
+			setStatus(Status.CLIENT_ERROR_NOT_FOUND, e.getMessage());
+		}
 		//String tmp = Job.INSTANCE.getJobInfo(job);		
-		return Job.INSTANCE.getJobInfo(job);
+		return null;
 	}
 	
 }
