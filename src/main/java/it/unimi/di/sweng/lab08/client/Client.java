d9965f2728dbe67c7c62b1584f429d668c1222ed
Giuliano Lubiana <giulianoflavio.lubiana@studenti.unimi.it>
2016-05-15 17:05:08 +0200

    prima versione main del client

--- src/main/java/it/unimi/di/sweng/lab08/client/Client.java
+++ src/main/java/it/unimi/di/sweng/lab08/client/Client.java
@@ -1,7 +1,10 @@
 package it.unimi.di.sweng.lab08.client;
 
 import java.util.Set;
+import java.util.Map.Entry;
+
 import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
 
 
 public class Client {
@@ -40,4 +43,54 @@ public class Client {
 		newJob.add();
 	}
 
+	public static void main(String args[]) {
+
+		if (args.length == 0) {
+			System.err.println("Missing command, available commands: jobs|job|newJob|endJob JOBS");
+			System.exit(-1);
+		}
+
+		final Client client = new Client(8080);
+		final String verb = args[0];
+		switch (verb) {
+
+		case "jobs":
+			try {
+				System.out.format("There are: ");
+				for (String job : client.jobs())
+					System.out.format("%s ", job);
+			} catch (ResourceException e) {
+				System.err.println("Server returned error: " + e.getMessage());
+			}
+			break;
+
+		case "newJob":
+			try {
+				client.newJob(args[1], args[2]);
+			} catch (ResourceException e) {
+				System.err.println("Server returned error: " + e.getMessage());
+			}
+			break;
+
+		case "job":
+			try {
+				System.out.println(client.job(args[1]));
+			} catch (ResourceException e) {
+				System.err.println("Server returned error: " + e.getMessage());
+			}
+			break;
+
+		case "endJob":
+			try {
+				client.endJob(args[1], args[2]);
+			} catch (ResourceException e) {
+				System.err.println("Server returned error: " + e.getMessage());
+			}
+			break;
+
+		default:
+			System.err.println("Unrecognized command, available commands: foods|eat FOOD|beverages|drink BEVERAGE");
+			break;
+		}
+	}
 }
